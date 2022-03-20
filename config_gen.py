b82d8c339b51b82a5b61f4d12b15be62f2491a80
Wong Shouhao <wongshouhao@outlook.com>
2022-03-20 09:35:47 +0800

    Finish config generator.

--- config_gen.py
+++ config_gen.py
@@ -1,7 +1,6 @@
-import requests
-import json
-import time
-
-area_api_url = 'http://libst.sdufe.edu.cn/api.php/v3areas/27'
-
 print('name\tid\n三楼\t28\n四楼\t29\n五楼\t30\n六楼\t31\n七楼\t32\n')
+
+order_id = input('请输入您预约楼层的id: ')
+
+with open('config.txt', 'w') as f:
+    f.write('http://libst.sdufe.edu.cn/web/seat2/area/{}/day/'.format(order_id))
