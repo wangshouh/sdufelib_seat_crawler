21898147ff12e5d0ded4271d97b56f5aa4c21c70
Wong Shouhao <wongshouhao@outlook.com>
2022-03-20 09:51:47 +0800

    Change info to json.

--- config_gen.py
+++ config_gen.py
@@ -1,6 +1,12 @@
+import json
+
 print('name\tid\n三楼\t28\n四楼\t29\n五楼\t30\n六楼\t31\n七楼\t32\n')
 
 order_id = input('请输入您预约楼层的id: ')
 
+info_dict = {
+    'prefix_url': 'http://libst.sdufe.edu.cn/web/seat2/area/{}/day/'.format(order_id),
+    'order_id': order_id
+}
 with open('config.txt', 'w') as f:
-    f.write('http://libst.sdufe.edu.cn/web/seat2/area/{}/day/'.format(order_id))
+    f.write(json.dumps(info_dict))
