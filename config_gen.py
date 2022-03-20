5d055e50dc91fc355ca3f2ba47134beee4a9c6e6
Wong Shouhao <wongshouhao@outlook.com>
2022-03-20 09:26:50 +0800

    Change area_info get.

--- config_gen.py
+++ config_gen.py
@@ -4,8 +4,4 @@ import time
 
 area_api_url = 'http://libst.sdufe.edu.cn/api.php/v3areas/27'
 
-headers = {"Referer" : "http://libst.sdufe.edu.cn/home/web/seat/area/27"}
-
-area_info = requests.get(area_api_url, headers=headers).json()
-
-print(area_info)
+print('name\tid\n三楼\t28\n四楼\t29\n五楼\t30\n六楼\t31\n七楼\t32\n')
