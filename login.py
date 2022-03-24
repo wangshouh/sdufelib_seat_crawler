cc8d6cc7a6b1de5415074210461cc84c4b6ccf4f
Wong Shouhao <wongshouhao@outlook.com>
2022-03-24 12:13:33 +0800

    Finish login.

---
+++ login.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_ticker(s):
+    """
+    获取lt参数,用于登录
+    """
+
+    url = 'http://ids.sdufe.edu.cn/authserver/login?service=http%3A%2F%2Flibst.sdufe.edu.cn%2Fcas%2Findex.php%3Fcallback%3Dhttp%3A%2F%2Flibst.sdufe.edu.cn%2Fhome%2Fweb%2Ff_second'
+    resp = s.get(url)
+    soup = BeautifulSoup(resp.text, 'lxml')
+    lt = soup.find("input", attrs={'name': "lt"}).attrs['value']
+    return lt
+
+def do_login(s, username, password, lt):
+    """
+    登入系统
+    """
+    url = 'http://ids.sdufe.edu.cn/authserver/login?service=http%3A%2F%2Flibst.sdufe.edu.cn%2Fcas%2Findex.php%3Fcallback%3Dhttp%3A%2F%2Flibst.sdufe.edu.cn%2Fhome%2Fweb%2Ff_second'
+    data = {'lt': lt,
+            'execution': 'e1s1',
+            '_eventId': 'submit',
+            'username': username,
+            'password': password,
+            'rmShown': '1'
+            }
+    resp = BeautifulSoup(s.post(url, data=data).text, 'lxml') 
+    if resp.body.a['href'] == 'http://libst.sdufe.edu.cn/home/web/f_second':
+        print('登录成功')
+    else:
+        print('登录失败')
+
+
